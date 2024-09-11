893debaf38c315cb470eb8a752f148f1c6315651
Aashir Jamil <168807577+Aashirdeveloper@users.noreply.github.com>
2024-09-11 15:59:56 -0600

    Using WP_Query
    In this we cover the WP_Query which used for displaying our own choice of post on a desired page.

--- WPlearning/template-home.php
+++ WPlearning/template-home.php
@@ -1,4 +1,3 @@
-
 <!-- 10-09-2024/Tuesday -->
 
 <?php
@@ -20,19 +19,81 @@ get_header();
 </div>
 
 
-<?php
-if (have_posts()): // its a function it can fetch all the posts hirerechly
-    while (have_posts()):
-        the_post();
-        ?>
-        <!-- // Display post content -->
 
-        <h1> <?php the_title(); ?> </h1>
-        <h3> <?php the_content(); ?> </h3>
-        <?php
-    endwhile;
-endif;
-?>
+<div class="flex-row ml-0 mr-0 mt-0 text-center">
+    <?php
+    if (have_posts()): // its a function it can fetch all the posts hirerechly
+        while (have_posts()):
+            the_post();
+            ?>
+            <!-- // Display post content -->
+
+            <!-- <h1> <?php the_title(); ?> </h1> -->
+            <?php the_content(); ?>
+            <?php
+        endwhile;
+    endif;
+    ?>
+</div>
+
+
+<!-- 11-09-2024/Wednesday -->
+<div class="home-posts row ml-0 mr-0 mt-5 ">
+    <div class="col">
+        <div class="section-head">
+            <h3>Latest from Technology</h3>
+        </div>
+        <div class="section-content">
+
+            <?php
+
+            $args = array(
+                'cat' => 7
+            );
+            $tech_posts = new WP_Query($args);
+
+            if ($tech_posts->have_posts()): // its a function it can fetch all the posts hirerechly
+                while ($tech_posts->have_posts()):
+                    $tech_posts->the_post();
+                    ?>
+
+                    <div class="home-post-row ml-0 mr-0 row-flex mb-3">
+                        <div class="post-thumb col-4 pl-0">
+                        <?php the_post_thumbnail('thumbnail', array('class' => 'img-fluid')) ?>
+
+                        </div>
+                        <div class="post-title col-8">
+                        <h3><a href="<?php echo get_the_permalink(get_the_ID()) ?>"><?php the_title() ?></a></h3>
+
+                        <p><?php  the_excerpt(); ?></p>
+                        </div>
+                       
+
+                    </div>
+
+                    <!-- // Display post content -->
+                  
+                    <?php
+                endwhile;
+            endif;
+            ?>
+            <?php wp_reset_postdata(); ?>
+
+
+
+        </div>
+    </div>
+
+
+
+    <div class="col">
+        <div class="section-head">
+            <h3>Latest from Social Media</h3>
+        </div>
+        <div class="section-content"></div>
+    </div>
+
+</div>
 
 
 
